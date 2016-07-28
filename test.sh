575d2c236c81bb399ff902a196ce271cd73c1dd9
tomeken-yoshinaga <kenta-yoshinaga@aist.go.jp>
2016-07-28 16:26:38 +0900

    [update] Install only crf++, and move compiling steps to test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,13 @@
 #!/bin/sh -e
 
-cd mecab
-./make
+cd ./mecab/mecab
+bash ./autogen.sh
+make
+make install
+ldconfig
+
+cd ../mecab-ipadic
+./configure --with-charset=utf8
+make
+make install
+ldconfig
